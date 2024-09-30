8303105e351355cd033ec32907e5b91e5fa71f4d
Alex <58120299+alex-bazzelle@users.noreply.github.com>
2024-09-30 11:50:10 -0500

    Update testedVigenere.py

--- testedVigenere.py
+++ testedVigenere.py
@@ -11,17 +11,17 @@ while(True):
 		result = "" # reset the result
 
 		for i in range(0,len(text)): # for each char
-			
-			if mode == "-e": # if encrypting, letter = (Pi + Ki)%26
-				letter = abc[(abc.index(text[i].lower()) + abc.index(key[i%len(key)]))%26]
-			elif mode == "-d": # if decrypting, letter = (26 + Ci - Ki)%26
-				letter = abc[(26 + abc.index(text[i].lower()) - abc.index(key[i%len(key)]))%26]
+			if text[i].isalpha(): # make sure it's alphabet
+				if mode == "-e": # if encrypting, letter = (Pi + Ki)%26
+					letter = abc[(abc.index(text[i].lower()) + abc.index(key[i%len(key)]))%26]
+				elif mode == "-d": # if decrypting, letter = (26 + Ci - Ki)%26
+					letter = abc[(26 + abc.index(text[i].lower()) - abc.index(key[i%len(key)]))%26]
 				
-			if text[i].upper() == text[i]: # if it's uppercase
-				letter = letter.upper() # make the result letter uppercase
+				if text[i].upper() == text[i]: # if it's uppercase
+					letter = letter.upper() # make the result letter uppercase
+			else: letter = text[i] # if not alphabet, just add it
 			result += letter # add to result
-			
-		sys.stdout.write(f"{result}\n") # print the result
+		sys.stdout.write(f"{result}\n")
 
 	except KeyboardInterrupt:
 		sys.stdout.write(f"\n")
