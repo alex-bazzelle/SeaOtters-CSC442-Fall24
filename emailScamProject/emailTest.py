9feb35bb8fd0f45f43e98fcf4da68747c070a165
Zanegerous <155862535+Zanegerous@users.noreply.github.com>
2024-10-09 15:06:56 -0500

    Added in the ability to loop through an excel sheet that will be used for our victim victimList once it is properly populated.

--- emailScamProject/emailTest.py
+++ emailScamProject/emailTest.py
@@ -1,14 +1,26 @@
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import smtplib
-import os
+from openpyxl import load_workbook
+
+# Opens the victim list workbook
+workbook = load_workbook(filename="emailScamProject/victimList.xlsx")
+sheet = workbook.active
+
+
+print(sheet.cell(row=1, column=1).value)
+print(sheet.cell(row=2, column=1).value)
+
+# Example showcasing how we can iterrate through each spot and use the data from the list
+for value in sheet.iter_rows(min_row=1, max_row=2, min_col=1,max_col=9,values_only=True):
+    print(value)
 
 # Notes: For testing purposes, change your desired email to your class email as I cant 2fa everyone into the scamproject email, but it should still
 #   work with the app password to send emails
 # Define email details
 FROM = 'ascamprojectforclass@gmail.com'
 TO = ["ascamprojectforclass@gmail.com"]  # lList of emails
-SUBJECT = "URGENT DATA BREACH!"
+SUBJECT = "sorryforspam"
 
 targetName = "Victum"
 
