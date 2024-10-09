008cd21e0f87743c905c7fe2cd4ff22472e0f4e1
Zanegerous <155862535+Zanegerous@users.noreply.github.com>
2024-10-09 11:56:11 -0500

    Updated so that we can format the script as html. provided an example of inputing from the python code with name

--- emailScamProject/emailTest.py
+++ emailScamProject/emailTest.py
@@ -1,26 +1,32 @@
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 import smtplib
+import os
 
 # Define email details
 FROM = 'ascamprojectforclass@gmail.com'
 TO = ["czu002@email.latech.edu"]  # lList of emails
-SUBJECT = "Hello!"
-TEXT = "This message was sent with Python's smtplib."
+SUBJECT = "URGENT DATA BREACH!"
 
-# Prepare actual message
-message = f"""\
-From: {FROM}
-To: {", ".join(TO)}
-Subject: {SUBJECT}
+targetName = "Cole"
 
-{TEXT}
-"""
+with open("emailScamProject/script.html", 'r') as file:
+    scriptText = file.read().format(name=targetName)
+
+message = MIMEMultipart()
+message['From'] = FROM
+message['To'] = ", ".join(TO)
+message['Subject'] = SUBJECT
+
+# Attach the formatted HTML content to the email
+message.attach(MIMEText(scriptText, 'html'))
 
 # Send the mail using Gmail's SMTP server
 try:
     server = smtplib.SMTP('smtp.gmail.com', 587)  # Gmail's SMTP server
     server.starttls()  # Secure the connection
     server.login(FROM, "nlrf sncc gskx lbqf")  # App Password Email
-    server.sendmail(FROM, TO, message)
+    server.sendmail(FROM, TO, message.as_string())
     print("Email sent successfully!")
 except Exception as e:
     print(f"Failed to send email: {e}")
