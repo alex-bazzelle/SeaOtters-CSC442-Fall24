ab93953686835980bb8fb0430e771e6a1c735b73
Ernesto Auerbach <95231139+Ernesto-Auerbach@users.noreply.github.com>
2024-10-30 22:54:45 -0500

    Update Timelock.py

--- Program Assignments/Timelock.py
+++ Program Assignments/Timelock.py
@@ -1,24 +1,50 @@
+import datetime
 import sys
+import hashlib
 
-# Just setup the inputs for later use
-# I believe we have to get the time elapsed from 0000 00 00 00 00 in minutes for each and sub the current from the old
-# that way we get the minute difference between. to determine the daylight savings im not so sure how it should
-# be done
+date_format = "%Y-%m-%d %H:%M:%S"
 
-CURRENT_TIME = "2017 03 23 18 02 06"
+current = datetime.datetime.now() #takes system time
+#debugging currentTime - sets system time 
+#current = datetime.datetime.strptime("2017-4-23 18:2:30", date_format)
+current = current - datetime.timedelta(hours=1, minutes=0)
 
-input = sys.stdin.readline().strip()
-cleanInput = input.strip('"')
+#reads epoch date input and converts to datetime obj
+epoch = "%s-%s-%s %s:%s:%s" % (int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4]), int(sys.argv[5]), int(sys.argv[6]))
+epoch = datetime.datetime.strptime(epoch, date_format)
 
-# Year Month Day Hour Minute Second
-# YY MM DD HH mm SS
-splitInput = cleanInput.split()
-splitCurrent = CURRENT_TIME.strip('"').split()
+#subtracts for timestamp
+difference = current - epoch
 
+#gets number of seconds passed
+diff_sec = difference.total_seconds() - (current.second + 1) #accounts for the intervals
 
-for x in splitInput:
-    print("TestInput = {}".format(x))
-for x in splitCurrent:
-    print("Test Current = {}".format(x))
-
+def condense_hash(hash):
+    j = 0
+    final_string = ""
+    for i in hash:
+        if not i.isdigit() and j < 2:
+            #print ("letter found! ", i)
+            final_string += i
+            j += 1
+    j = 0
+    for i in reversed(hash):
+        if i.isdigit() and j < 2:
+            #print ("number found! ", i)
+            final_string += i
+            j += 1
     
+    return final_string
+
+#gets hash of difference
+#Have been trying different combos of numbers and 'big' or 'little'
+# was supposed to encode it as a string, round to the start of the interval, and hash it twice
+diff_sec_int = int(diff_sec)
+diff_sec_str = str(diff_sec_int)
+hash1 = hashlib.md5(diff_sec_str.encode()).hexdigest()
+hash2 = hashlib.md5(hash1.encode()).hexdigest()
+#hashed_date = hashlib.md5(diff_sec_int.to_bytes(7, 'big', signed=True)).hexdigest()
+#print("hash1: {}".format(hash1))
+#print("hash2: {}".format(hash2))
+#print ("hashed date: ", hashed_date)
+print (condense_hash(hash2))
