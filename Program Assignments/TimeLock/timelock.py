1de7e46285aab4f5473c095e244c0fb393193178
Alex <58120299+alex-bazzelle@users.noreply.github.com>
2024-11-04 12:04:19 -0600

    idk but it works now

--- Program Assignments/TimeLock/timelock.py
+++ Program Assignments/TimeLock/timelock.py
@@ -2,49 +2,56 @@ import datetime
 import sys
 import hashlib
 
+
+timeDelta = -1
+
+
+
 date_format = "%Y-%m-%d %H:%M:%S"
 
 current = datetime.datetime.now() #takes system time
 #debugging currentTime - sets system time 
 #current = datetime.datetime.strptime("2017-4-23 18:2:30", date_format)
-current = current - datetime.timedelta(hours=1, minutes=0)
+#current = datetime.datetime(2015,1,1,0,0,0)
+current = current - datetime.timedelta(hours=timeDelta, minutes=0)
 
 #reads epoch date input and converts to datetime obj
 epoch = "%s-%s-%s %s:%s:%s" % (int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4]), int(sys.argv[5]), int(sys.argv[6]))
 epoch = datetime.datetime.strptime(epoch, date_format)
 
+
+
 #subtracts for timestamp
 difference = current - epoch
 
 #gets number of seconds passed
-diff_sec = difference.total_seconds() - (current.second + 1) #accounts for the intervals
+
+secondDelta = 0
+if(current.second > epoch.second): secondDelta = current.second-epoch.second
+if(current.second < epoch.second): secondDelta = 60 - (epoch.second - current.second)
+diff_sec = difference.total_seconds() - secondDelta #accounts for the intervals
 
 def condense_hash(hash):
-    j = 0
+    foundCount = 0
     final_string = ""
-    for i in hash:
-        if not i.isdigit() and j < 2:
+    for letter in hash:
+        if not letter.isdigit() and foundCount < 2:
             #print ("letter found! ", i)
-            final_string += i
-            j += 1
-    j = 0
-    for i in reversed(hash):
-        if i.isdigit() and j < 2:
+            final_string += letter
+            foundCount += 1
+    foundCount = 0
+    for number in reversed(hash):
+        if number.isdigit() and foundCount < 2:
             #print ("number found! ", i)
-            final_string += i
-            j += 1
+            final_string += number
+            foundCount += 1
     
     return final_string
 
 #gets hash of difference
-#Have been trying different combos of numbers and 'big' or 'little'
-# was supposed to encode it as a string, round to the start of the interval, and hash it twice
 diff_sec_int = int(diff_sec)
 diff_sec_str = str(diff_sec_int)
 hash1 = hashlib.md5(diff_sec_str.encode()).hexdigest()
 hash2 = hashlib.md5(hash1.encode()).hexdigest()
-#hashed_date = hashlib.md5(diff_sec_int.to_bytes(7, 'big', signed=True)).hexdigest()
-#print("hash1: {}".format(hash1))
-#print("hash2: {}".format(hash2))
-#print ("hashed date: ", hashed_date)
-print (condense_hash(hash2))
+print("Full Hash: {}".format(hash2))
+print ("Code: {}{}".format(condense_hash(hash2),hash2[(len(hash2)//2)]))
